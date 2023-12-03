10bea395b2029918f132dcbf6f4f53907d138db1
Bartosz Berdychowski <b.berdychowski@gmail.com>
2023-12-03 22:20:25 +0100

    add swiper

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 import "lazysizes";
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+
+const swiperBusiness = new Swiper(".swiper-business", {
+    slidesPerView: "auto",
+    modules: [Navigation],
+    navigation: {
+        nextEl: ".swiper-business-next",
+        prevEl: ".swiper-business-prev"
+    }
+});
+
+const swiperWork = new Swiper(".swiper-work", {
+    slidesPerView: "auto",
+    modules: [Navigation],
+    navigation: {
+        nextEl: ".swiper-work-next",
+        prevEl: ".swiper-work-prev"
+    }
+});
